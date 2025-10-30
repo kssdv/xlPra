8c9c60c251c2e136063c41938595d13b14eb9681
Kim Sangsin <kssdv0427@gmail.com>
2025-10-30 15:52:31 +0900

    start xlPra project

---
+++ src/main/java/org/example/xlPra/XlPraApplication.java
@@ -0,0 +1,13 @@
+package org.example.xlPra;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XlPraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(XlPraApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/example/xlPra/XlPraApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.xlPra;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class XlPraApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
